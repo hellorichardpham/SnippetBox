cca18cdcf28e7b1700456df1928df0cb31575b0f
Richard Pham <hellorichardpham@gmail.com>
2020-12-16 14:08:23 -0800

    implemented INSERT statement

---
+++ pkg/models/mysql/snippets.go
@@ -0,0 +1,41 @@
+package mysql
+
+import (
+	"database/sql"
+
+	"github.com/hellorichardpham/SnippetBox/pkg/models"
+)
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	// Write the SQL statement we want to execute. I've split it over two lines
+	// for readability (which is why it's surrounded with backquotes instead
+	// of normal double quotes).
+	stmt := `INSERT INTO Snippets (title, content, created, expires)
+	VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP, INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use the LastInsertId() method on the result object to get the ID of our
+	// newly inserted record in the snippets table.
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	return nil, nil
+}
+
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+	return nil, nil
+}
